8b008095c8769efdff5e8623764791e1c94e906c
WaiMaungMaung <mgwaimaungmaung@gmail.com>
2021-02-03 09:28:31 +0630

    login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,19 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet" />
     <title> @yield('title')</title>
 </head>
 
 <body>
 
-    @section('sidebar')
-        This is the master sidebar.
+    @section('nav')
+        <div class="container md:px-36 py-5">
+            <div class="grid gap-5 md:gap-x-72 grid-cols-3">
+                <img src="images/Canon.png" class="col-start-1" alt="img">
+                <img src="images/cpmlogo.png" alt="img">
+                <img src="images/MGR.png" alt="img">
+
+            </div>
+        </div>
     @show
 
-    <div class="container">
+    <div class="container" id="contents"
+        style='background-image: url(" images/For JUDGE App_1.jpg");background-size:cover;height:1200px;'>
         @yield('content')
     </div>
 </body>
--- resources/views/main_view.blade.php
+++ resources/views/main_view.blade.php
@@ -2,31 +2,13 @@
 
 @section('title', 'Page Titles')
 
-@section('sidebar')
-    @parent
 
-    <p>This is appended to the master sidebar.</p>
-@stop
 
 @section('content')
-    <figure class="bg-gray-200 rounded-xl p-8 grid grid-cols-12">
-        <img class="w-32 h-32 rounded-full mx-auto" src="images/svg.png" alt="" width="384" height="512">
-        <div class="pt-6 space-y-4">
-            <blockquote>
-                <p class="text-lg font-semibold text-red-300">
-                    “Tailwind CSS is the only framework that I've seen scale
-                    on large teams. It’s easy to customize, adapts to any design,
-                    and the build size is tiny.”
-                </p>
-            </blockquote>
-            <figcaption class="font-medium">
-                <div class="text-blue-300">
-                    Sarah Dayan
-                </div>
-                <div class="text-gray-500">
-                    Staff Engineer, Algolia
-                </div>
-            </figcaption>
-        </div>
-    </figure>
+    <div class="grid place-items-center" style="width:auto; height:500px; ">
+        <a href=""
+            class="border-2 border-blue-300 font-bold text-blue-300 px-4 py-3 transition duration-300 ease-in-out rounded-lg hover:bg-blue-400 hover:text-white mr-6 md:px-72">
+            Click here
+        </a>
+    </div>
 @stop
